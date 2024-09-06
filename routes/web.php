4c93a92b93705b85e000bc0e8eae46a61d970ab1
aisyahandini <aisyahh33ais@gamil.com>
2024-09-06 13:21:39 +0700

    menambahkan_halaman login

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,8 @@
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', function () {
-    return view('register');
+Route::get('/', function () {
+    return view('landingpage');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
 
